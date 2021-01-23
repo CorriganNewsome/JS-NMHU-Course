fa2c81863bf64302d557346297ddc6ad944392cb
Corrigan Newsome <corrigannewsome@gmail.com>
2021-01-23 16:06:02 -0700

    step 1-3 in homework example

--- CN-BSSD5520-2.1 /main.js
+++ CN-BSSD5520-2.1 /main.js
@@ -1,2 +1,25 @@
-// eslint-disable-next-line no-undef
-document.addEventListener('DOMContentLoaded', init);
+'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+	//look up an element in the document hierarchy by id
+	const greeting = document.getElementById('greeting');
+	const name = prompt('Please enter your name.');
+	greeting.textContent = name;
+});
+
+function createGreetingHTML() {
+	//create html tag elements in memory
+	const divider = document.createElement('div');
+	const paragraph = document.createElement('p');
+	const emphasis = document.createElement('em');
+	//append tags in a parent-child hierarchy
+	divider.append(paragraph);
+	//set inner content of the p tag
+	paragraph.textContent = 'Hello, ';
+	paragraph.append(emphasis);
+	//set inner content of the em tag
+	emphasis.textContent = name;
+	paragraph.append('. How is your day?');
+
+	//append the parent div to the document's pre-existing body tag.
+	document.body.append(divider);
+}
