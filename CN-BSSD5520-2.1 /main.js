8c5c5d38e1c65ec3ff857aca6c83d4e9302f9ac9
Corrigan Newsome <corrigannewsome@gmail.com>
2021-01-23 16:38:30 -0700

    finished the homework except for changing the text color

--- CN-BSSD5520-2.1 /main.js
+++ CN-BSSD5520-2.1 /main.js
@@ -2,24 +2,26 @@
 document.addEventListener('DOMContentLoaded', () => {
 	//look up an element in the document hierarchy by id
 	const greeting = document.getElementById('greeting');
-	const name = prompt('Please enter your name.');
-	greeting.textContent = name;
+	const streetName = getInput('Please enter your street name.');
+	const petName = getInput('Please enter your pet name.');
+	const favColor = getInput('Which color is better (Blue,Green, or Red)');
+	const favLanguage = getInput('What is your favorite programming language?');
+	greeting.textContent =
+		'Oh, you must be the famous' +
+		' ' +
+		favLanguage +
+		' ' +
+		'hacker' +
+		' ' +
+		petName +
+		' ' +
+		streetName;
 });
 
-function createGreetingHTML() {
-	//create html tag elements in memory
-	const divider = document.createElement('div');
-	const paragraph = document.createElement('p');
-	const emphasis = document.createElement('em');
-	//append tags in a parent-child hierarchy
-	divider.append(paragraph);
-	//set inner content of the p tag
-	paragraph.textContent = 'Hello, ';
-	paragraph.append(emphasis);
-	//set inner content of the em tag
-	emphasis.textContent = name;
-	paragraph.append('. How is your day?');
-
-	//append the parent div to the document's pre-existing body tag.
-	document.body.append(divider);
-}
+function getInput(prompText) {
+	let name = prompt(prompText);
+	while (name === '') {
+		name = prompt('Cannot Leave Blank: ' + prompText);
+	}
+	return name;
+} //end function getInput(promptText)
