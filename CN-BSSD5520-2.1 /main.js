fa2121fba687bb3012becef689c83125d2a29a86
Corrigan Newsome <corrigannewsome@gmail.com>
2021-01-26 13:35:43 -0700

    figured out how to capitlize the first letter in a string, and added color to just the name

--- CN-BSSD5520-2.1 /main.js
+++ CN-BSSD5520-2.1 /main.js
@@ -2,20 +2,26 @@
 document.addEventListener('DOMContentLoaded', () => {
 	const greeting = document.getElementById('greeting');
 	const streetName = getInput('Please enter your street name.');
-	const petName = getInput('Please enter your pet name.');
-	var favColor = getInput('Which color is better (Blue,Green, or Red)');
+	const petName = getInput('Please enter your pet name.').toLowerCase();
+
+	var favColor = getInput(
+		'Which color is better (Blue,Green, or Red)',
+	).toLowerCase();
+
 	const favLanguage = getInput('What is your favorite programming language?');
-	const name = petName + favColor;
+	const name = petName.capitlize() + ' ' + streetName.capitlize();
 
 	if (favColor === 'blue' || favColor === 'red' || favColor === 'green') {
-		greeting.textContent =
+		document.write(
 			'Oh, you must be the famous' +
-			' ' +
-			favLanguage +
-			' ' +
-			'hacker' +
-			' ' +
-			name.fontcolor(favColor);
+				' ' +
+				favLanguage +
+				' ' +
+				'hacker' +
+				'<em>' +
+				' ' +
+				name.fontcolor(favColor),
+		);
 	} else {
 		greeting.textContent =
 			'Oh, you must be the famous' +
@@ -31,6 +37,10 @@ document.addEventListener('DOMContentLoaded', () => {
 		h2.textContent = 'Access Granted';
 	}
 });
+
+String.prototype.capitlize = function () {
+	return this.charAt(0).toUpperCase() + this.slice(1);
+};
 
 function getInput(prompText) {
 	let name = prompt(prompText);
